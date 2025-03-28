10a4764dd0896760f66e6c9e57fb0523db3d84e2
Chathura Matheesha <chathuramatheesha@gmail.com>
2025-03-28 16:38:41 +0530

    add users table to sqlalchemy orm (models.py -> User)

--- app/config.py
+++ app/config.py
@@ -2,6 +2,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class BasicConfig(BaseSettings):
+    SECRET_KEY: str | None = None
+    ALGORITHM: str | None = None
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     DATABASE_URL: str | None = None
     DATABASE_ECHO: bool = False
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
--- app/db/models.py
+++ app/db/models.py
@@ -1,8 +1,25 @@
-from sqlalchemy import String, Integer, Boolean, DateTime
-from sqlalchemy.orm import Mapped, mapped_column
+import enum
+
+from sqlalchemy import String, Integer, Boolean, DateTime, ForeignKey, Enum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime, timezone
 
 from app.db.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, index=True, autoincrement=True
+    )
+    fullname: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, nullable=False, unique=True, index=True)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    tasks: Mapped[list["Task"]] = relationship("Task", back_populates="user")
+
+    def get_first_name(self) -> str:
+        return self.fullname.split(" ")[0]
 
 
 class Task(Base):
@@ -18,6 +35,8 @@ class Task(Base):
         DateTime, nullable=False, default=datetime.now(timezone.utc)
     )
     due_date: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("userrs.id"), nullable=False)
+    user: Mapped["User"] = relationship("User", back_populates="tasks")
 
     def __repr__(self):
         return f"<Task(id={self.id}, title='{self.title}', is_complete={self.is_complete}, created_at={self.created_at}, due_date={self.due_date})>"
